5349da426da5651c2f7d7c52a4e58ff9c39b8b8b
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-01-23 15:42:00 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
+[![Build Status](https://travis-ci.org/zrrrzzt/firebase-counter.svg?branch=master)](https://travis-ci.org/zrrrzzt/firebase-counter)
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
 # firebase-save
-Save to firebase
+Save data to [firebase](https://firebase.google.com)
+
+## Installation
+```bash
+$ npm i firebase-saev --save
+```
+
+## Usage
+Saves and loookups values for a specific key in firebase.
+- Uses ```value``` as key if no key supplied.
+- Defaults to {} if no value is presented.
+- Supports promises and callbacks
+
+```JavaScript
+const fbs = require('firebase-save')
+const options = {
+  apiKey: '<your-api-key>',
+  authDomain: '<your-auth-domain>',
+  databaseURL: '<your-database-url>',
+  appName: '<your-app-name>',
+  authEmail: '<your-auth-email>',
+  authPassword: '<your-auth-password>'
+}
+
+const database = fbs(options)
+
+const someData = {
+  key: 'somedata',
+  value: {
+    fishy: true,
+    birdy: true,
+    ugly: {
+      facts: 'alternative'
+    }
+  }
+}
+
+database.save(someData).then(data => console.log(data))
+
+database.lookup({'key': 'somedata'}).then(data => console.log(data))
+```
+
+## License
+[MIT](LICENSE)
+
+![alt text](https://robots.kebabstudios.party/firebase-save.png "Robohash image of firebase-save")
---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict'
+
+module.exports = (options) => {
+  const firebase = require('firebase')
+  const app = firebase.initializeApp({
+    apiKey: options.apiKey,
+    authDomain: options.authDomain,
+    databaseURL: options.databaseURL
+  }, options.appName)
+
+  if (options.authEmail && options.authPassword) {
+    app.auth().signInWithEmailAndPassword(options.authEmail, options.authPassword).catch(error => {
+      return error
+    })
+  }
+
+  const database = app.database()
+
+  const save = (args, callback) => {
+    return new Promise((resolve, reject) => {
+      const selectedKey = args.key || 'value'
+      const value = args.value || {}
+      const valueRef = database.ref(selectedKey)
+      valueRef.set(value)
+        .then(() => {
+          const result = {key: selectedKey, value: value}
+          if (callback) {
+            return callback(null, result)
+          }
+          resolve(result)
+        })
+        .catch((error) => {
+          if (callback) {
+            return callback(error, null)
+          }
+          reject(error)
+        })
+    })
+  }
+
+  const lookup = (args, callback) => {
+    return new Promise((resolve, reject) => {
+      const selectedKey = args.key || 'value'
+      database.ref(selectedKey).once('value').then((snapshot) => {
+        const value = snapshot.val()
+        const result = {key: selectedKey, value: value}
+        if (callback) {
+          return result
+        }
+        resolve(result)
+      }).catch((error) => {
+        reject(error)
+      })
+    })
+  }
+
+  return {
+    save: save,
+    lookup: lookup
+  }
+}
